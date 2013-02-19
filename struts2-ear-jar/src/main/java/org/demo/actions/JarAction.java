d81f38f4d0474ae266fa707c2c3a17c6313ffaf1
lukaszlenart <lukasz.lenart@gmail.com>
2013-02-19 14:26:34 +0100

    Adds additional project to demonstrate how to add support for actions inside jar which is in war archive and put in ear archive

---
+++ struts2-ear-jar/src/main/java/org/demo/actions/JarAction.java
@@ -0,0 +1,8 @@
+package org.demo.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.convention.annotation.Action;
+
+@Action("/jarjar")
+public class JarAction extends ActionSupport {
+}
