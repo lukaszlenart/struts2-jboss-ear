0120bb67ae7cc2326faaf13621d762228a945586
lukaszlenart <lukasz.lenart@gmail.com>
2013-02-21 12:47:04 +0100

    Adds additional jar project to prove there is no issue with that

---
+++ struts2-ear-jar2/src/main/java/org/demo/actions/Jar2Action.java
@@ -0,0 +1,8 @@
+package org.demo.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.convention.annotation.Action;
+
+@Action("/jar2jar")
+public class Jar2Action extends ActionSupport {
+}
---
+++ struts2-ear-web/src/main/webapp/WEB-INF/content/jar2jar.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<html>
+<head>
+    <title>Jar Action</title>
+</head>
+
+<body>
+
+Jar
+
+</body>
+</html>
